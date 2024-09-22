d2cde0e61de37828448c39fdfefe8d49dba17f0f
A. Blakeslee Moore <andrew@abkslm.com>
2024-09-22 12:43:49 -0700

    Updated determination tests and test output

--- model-testers/hybrid_determination_tester.py
+++ model-testers/hybrid_determination_tester.py
@@ -17,9 +17,14 @@ explicit = 'explicit'
 labels = [offline_q, basic_q, complex_q, vision, explicit]
 
 questions = test_reader.load_queries("./tests/determination.csv")
+counts = {
+    "offline_question": {"correct": 0, "failures": 0},
+    "basic_question": {"correct": 0, "failures": 0},
+    "complex_question": {"correct": 0, "failures": 0},
+    "vision": {"correct": 0, "failures": 0},
+    "explicit": {"correct": 0, "failures": 0}
+}
 
-hits = 0
-total = len(questions)
 
 print(f"\nGetting answers...\n")
 for question in questions:
@@ -35,15 +40,34 @@ for question in questions:
 
     correct = False
     if labels[predicted_label] == question['label']:
+        counts[question['label']]['correct'] += 1
         correct = True
-        hits += 1
+    else:
+        counts[question['label']]['failures'] += 1
 
-    # Print the result
+    if not correct:
+        print(
+            f"{str("\033[92m" +"PASS" + "\033[0m") if correct else str("\033[91m" +"FAIL" + "\033[0m")} Question: {question['query']}"
+            f"\n\t\tPredicted label: {labels[predicted_label]}"
+            f"\n\t\tExpected label:  {question['label']}"
+        )
+
+
+print()
+for count in counts:
     print(
-        f"{str("\033[92m" +"PASS" + "\033[0m") if correct else str("\033[91m" +"FAIL" + "\033[0m")} Question: {question['query']}"
-        f"\n\t\tPredicted label: {labels[predicted_label]}"
-        f"\n\t\tExpected label:  {question['label']}"
+        f"{count}:"
+        f"\n\t{str("\033[92m" +"CORRECT" + "\033[0m")}: {counts[count]['correct']}"
+        f"\n\t{str("\033[91m" +"FAILURES" + "\033[0m")}: {counts[count]['failures']}"
     )
 
-print(f"\nHits: {hits}, Misses: {total - hits}")
+total_correct = sum(count["correct"] for count in counts.values())
+total_failures = sum(count["failures"] for count in counts.values())
+total_responses = total_correct + total_failures
+
+print(
+    f"\nTotal Correct: {total_correct}"
+    f"\nTotal Failures: {total_failures}"
+    f"\nPercent correct: {(total_correct / total_responses) * 100:.1f}%"
+)
 
