da404303b88aeddb14f0e523145c0b2a51553972
A. Blakeslee Moore <andrew@abkslm.com>
2024-11-04 13:43:14 -0800

    Updating WhisperConnector.py skeleton

--- WhisperConnector.py
+++ WhisperConnector.py
@@ -1,5 +1,18 @@
 # not sure what needs to be imported, have fun :)
 
+
 class WhisperConnector:
     def __init__(self):
+        # TODO add instance vars/members if necessary
         pass
+
+    # TODO other functions here.
+
+    def test(self):
+        # TODO use this for basic tests (a janky unit-test)
+        pass
+
+
+if __name__ == '__main__':
+    whisper = WhisperConnector()
+    whisper.test()
